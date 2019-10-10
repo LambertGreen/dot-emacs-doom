054ca0e43f4ee7d7df686928ca37738356871274
Lambert Green <lgreen@bxavier-mac5.tsi.lan>
2019-10-10 13:44:47 -0700

    Setting separate font entries per platform.

--- config.el
+++ config.el
@@ -1,4 +1,17 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq doom-font (font-spec :family "Hack Nerd Font" :size 15))
+
+;; Set font
+(if (eq system-type 'gnu/linux)
+    (setq
+     doom-font (font-spec :family "Hack Nerd Font" :size 15)
+    ))
+(if (eq system-type 'darwin)
+    (setq
+     doom-font (font-spec :family "Hack Nerd Font" :size 15)
+    ))
+(if (eq system-type 'windows-nt)
+    (setq
+     doom-font (font-spec :family "Hack NF" :size 15)
+    ))
