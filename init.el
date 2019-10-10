ac82b00a46d12c6f47532adea9d6d5d67f64786d
Lambert Green <lgreen@lgreen-lx.tsi.lan>
2019-10-10 12:08:39 -0700

    Adding files to org-agenda-files.

--- init.el
+++ init.el
@@ -176,3 +176,15 @@
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/dev/my/notes/work.org" "~/dev/my/notes/personal.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
