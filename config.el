20a61436439a9754551160cc6eeb4fb46dda1e30
Lambert Green <lgreen@tableau.com>
2019-12-13 10:29:21 -0800

    Set find program for Windows.

--- config.el
+++ config.el
@@ -1,17 +1,21 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+
+;; Set find program
+(if (eq system-type 'windows-nt)
+    (setq find-program "~/scoop/shim/find.exe")
+  )
 
 ;; Set font
 (if (eq system-type 'gnu/linux)
     (setq
      doom-font (font-spec :family "Hack Nerd Font" :size 15)
-    ))
+     ))
 (if (eq system-type 'darwin)
     (setq
      doom-font (font-spec :family "Hack Nerd Font" :size 15)
-    ))
+     ))
 (if (eq system-type 'windows-nt)
     (setq
      doom-font (font-spec :family "Hack NF" :size 15)
-    ))
+     ))
