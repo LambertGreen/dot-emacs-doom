beece4e145712a5f962f8ce01b63f5f5cdbc9ec3
Lambert Green <lambert_green@msn.com>
2020-12-26 20:46:22 -0800

    Removing transparency

--- config.el
+++ config.el
@@ -189,8 +189,9 @@
 (eval-after-load 'flyspell '(define-key flyspell-mode-map "\M-\t" nil))
 
 ;; Transparency
-(set-frame-parameter (selected-frame) 'alpha '(95 95))
-(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
+;; TODO: Decide whether you want transparency back or not
+;; (set-frame-parameter (selected-frame) 'alpha '(95 95))
+;; (add-to-list 'default-frame-alist '(alpha . (95 . 95)))
 
 ;; Workaround ripgrep issue on Windows
 (if (eq system-type 'windows-nt)
