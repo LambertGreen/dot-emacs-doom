29638f359b2aa290effb275b1aa8cccef2e3e799
Lambert Green <lambert_green@msn.com>
2021-05-03 14:51:19 -0700

    Adds ligatures and minimap

--- init.el
+++ init.el
@@ -34,6 +34,8 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
+       ligatures         ; ligatures and symbols to make your code pretty again
+       minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
        ;;neotree           ; a project drawer, like NERDTree for vim
