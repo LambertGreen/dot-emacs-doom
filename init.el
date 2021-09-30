9b2f5df73490d2030009df6463c9c3e89c5fcd9f
Lambert Green <lambert_green@msn.com>
2021-09-29 17:05:08 -0700

    Set ASpell as the spell checker

--- init.el
+++ init.el
@@ -83,9 +83,11 @@
        vterm             ; another terminals in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell)   ; tasing you for misspelling mispelling
-       ;;grammar           ; tasing grammar mistake every you make
+       syntax            ; tasing you for every semicolon you forget
+       (spell            ; tasing you for misspelling mispelling
+        +flyspell
+        +aspell)
+       ;;grammar         ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
