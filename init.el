aee87fae7a7b5e06e53ab1409b42e1fccab86611
Lambert Green <lambert_green@msn.com>
2021-11-10 14:32:48 -0800

    Switching out ivy for vertico

--- init.el
+++ init.el
@@ -22,9 +22,7 @@
        company            ; the ultimate code completion backend
        ;;helm             ; the *other* search engine for love and life
        ;;ido              ; the other *other* search engine...
-       (ivy               ; a search engine for love and life
-        +fuzzy
-        +prescient
+       (vertico           ; the search engine of the future
         +icons)
 
        :ui
