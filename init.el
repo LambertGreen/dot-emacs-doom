37ad8b0e8020274fdf0b05d64548357ec6e4dc05
Lambert Green <lambert_green@msn.com>
2023-07-24 14:17:42 -0700

    Add extra ligatures
    Actually got to looking into ligatures because there was an issue
    showing up when using Emacs when install via Homebrew from HEAD (on
    Ubuntu). Fix was to use Emacs from `ppa:ubuntu-elisp`.

--- init.el
+++ init.el
@@ -54,6 +54,7 @@
        hydra
        indent-guides     ; highlighted indent columns
        (ligatures        ; ligatures and symbols to make your code pretty again
+        +extra
         +iosevka)
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
